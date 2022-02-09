e59e9571a275dd2be4b9365fea40b2a4dd0ff315
Shishir Shetty <shishirshetty0710@gmail.com>
2022-02-09 15:44:25 -0500

    Hook updated
    Hook updated to edit Manifest file

--- hooks/outsystems/handle_cleartextTrafficPermitted.js
+++ hooks/outsystems/handle_cleartextTrafficPermitted.js
@@ -27,6 +27,31 @@ module.exports = function(context) {
                 }
             });
         }
+		var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+		var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
+		
+		if (fs.existsSync(manifestFile)) {
+			fs.readFile(manifestFile, 'utf8', function (err,data) {
+				if (err) {
+					throw new Error('Unable to find AndroidManifest.xml: ' + err);
+				}
+				var allowBackup = 'android:allowBackup=';
+				var allowBackupVal = 'false';
+				var allowBackupOrig = 'android:allowBackup="true"';
+				var result = '';
+				if(data.indexOf(allowBackup) == -1) {
+					 result = data.replace(/<application/g, '<application android:allowBackup="' + allowBackupVal + '"');
+				} else{
+					result = data.replace(allowBackupOrig, 'android:allowBackup="' + allowBackupVal + '"');
+				}
+				fs.writeFile(manifestFile, result, 'utf8', function (err) {
+                        if (err) {
+                            throw new Error('Unable to write into AndroidManifest.xml: ' + err);
+                        }
+                    })
+			});
+		}
+		
 
         return resolve();
     });
